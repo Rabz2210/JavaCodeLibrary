310d1fd84ab24b16f12e7b9c52bb220508742e3e
Rabz2210 <topspin2210@gmail.com>
2020-08-20 14:37:27 +0530

    initialCommit

---
+++ com/trees/Node.java
@@ -0,0 +1,20 @@
+package com.trees;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+}
