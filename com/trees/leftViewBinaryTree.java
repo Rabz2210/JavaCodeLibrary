c4c1f5283c8cfaaf724917d786dd378863b456f7
Rabz2210 <topspin2210@gmail.com>
2020-08-24 01:17:17 +0530

    initial commit

--- com/trees/leftViewBinaryTree.java
+++ com/trees/leftViewBinaryTree.java
@@ -1,6 +1,29 @@
+//Iterative Approch
+
 package com.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class leftViewBinaryTree {
+    public static void leftVBinary(Node root){
+        Queue<Node> q = new LinkedList<Node>();
+        if(root!=null)q.add(root);
+        while(q.peek()!=null){
+            int count = q.size();
+            for(int i=0;i<count;i++){
+                if(i==0){
+                    System.out.println(" "+root.data);
+                }
+                if(root.left!=null){
+                    q.add(root.left);
+                }
+                if(root.right!=null){
+                    q.add(root.right);
+                }
+            }
+        }
+    }
     
     
 }
