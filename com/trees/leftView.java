7686e017ed9d95e38989da8429ff45f40a08e4d1
Rabz2210 <topspin2210@gmail.com>
2020-08-23 23:44:00 +0530

    main method

--- com/trees/leftView.java
+++ com/trees/leftView.java
@@ -12,4 +12,13 @@ public class leftView {
         lfView(root.right, level++);
     }
 
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        lfView(root,1);
+    }
+
 }
