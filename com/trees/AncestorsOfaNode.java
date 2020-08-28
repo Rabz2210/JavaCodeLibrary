f32fe36258aeae42983e709dad91c5f5ce0ee71a
Rabz2210 <topspin2210@gmail.com>
2020-08-28 12:03:44 +0530

    commit

--- com/trees/AncestorsOfaNode.java
+++ com/trees/AncestorsOfaNode.java
@@ -9,5 +9,13 @@ public class AncestorsOfaNode {
         }
         return false;
     }
-    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.left.left = new Node(7);
+        anstr(root, 3);
+    }
 }
