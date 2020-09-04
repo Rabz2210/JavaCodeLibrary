f72d3cbb985671f0b58fd15eee61d461e81f31dd
Rabz2210 <topspin2210@gmail.com>
2020-09-04 23:53:20 +0530

    initial Commit

---
+++ com/datastructures/linkedList.java
@@ -0,0 +1,29 @@
+package com.datastructures;
+
+import java.util.Scanner;
+
+public class linkedList {
+   static Node head;
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] ele = scan.nextLine().split(" ");
+        if(ele[0]!=null)
+        for(String each:ele){
+            Node temp = new Node(Integer.parseInt(each));
+        }
+        
+
+    }
+    
+}
