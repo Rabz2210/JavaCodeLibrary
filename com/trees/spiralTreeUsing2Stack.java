314c1ba094c1f61a34dc59042e7ddfa0f4cbad28
Rabz2210 <topspin2210@gmail.com>
2020-09-06 01:12:42 +0530

    initial commit

--- com/trees/spiralTreeUsing2Stack.java
+++ com/trees/spiralTreeUsing2Stack.java
@@ -24,6 +24,27 @@ public class spiralTreeUsing2Stack {
         }
     }
     public static void main(String[] args) {
-        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.left.left.left = new Node(8);
+        root.left.left.right = new Node(9);
+        root.right.left.left = new Node(10);
+        spiralTree(root);
+
+        System.out.println(" ");
+        Node root2 = new Node(10);
+        root2.left = new Node(20);
+        root2.right = new Node(30);
+        root2.right.left = new Node(40);
+        root2.right.right = new Node(50);
+        root2.right.left.left = new Node(60);
+        root2.right.left.right = new Node(70);
+        root2.right.right.right = new Node(80);
+        spiralTree(root2);
     }
 }
