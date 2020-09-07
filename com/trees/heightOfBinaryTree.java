9d0b4626a698776cb1c53af665299e7eb8b4f2ab
Rabz2210 <topspin2210@gmail.com>
2020-09-07 11:07:52 +0530

    initial Commit

--- com/trees/heightOfBinaryTree.java
+++ com/trees/heightOfBinaryTree.java
@@ -4,8 +4,7 @@ public class heightOfBinaryTree {
 
     public static int hob(Node root){
         if(root==null)return 0;
-        int height=1;
-        return (height+Math.max(hob(root.left), hob(root.right)));
+        return (1+Math.max(hob(root.left), hob(root.right)));
     }
     
     public static void main(String[] args) {
