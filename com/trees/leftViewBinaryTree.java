88150875474e15a275eed73285add66aee30b086
Rabz2210 <topspin2210@gmail.com>
2020-09-07 11:08:05 +0530

    Initial Commit

--- com/trees/leftViewBinaryTree.java
+++ com/trees/leftViewBinaryTree.java
@@ -6,24 +6,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class leftViewBinaryTree {
-    public static void leftVBinary(Node root){
-        Queue<Node> q = new LinkedList<Node>();
-        if(root!=null)q.add(root);
+    public static void lfview(Node root){
+        if(root==null)System.out.println("Tree is empty");
+        Queue<Node>q = new LinkedList<Node>();
+        q.add(root);
         while(q.peek()!=null){
-            int count = q.size();
-            for(int i=0;i<count;i++){
+            Node temp;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
                 if(i==0){
-                    System.out.println(" "+root.data);
+                    temp=q.poll();
+                    System.out.print(" "+temp.data);
+                    if(temp.left!=null)q.add(temp.left);
+                    if(temp.right!=null)q.add(temp.right);
                 }
-                if(root.left!=null){
-                    q.add(root.left);
-                }
-                if(root.right!=null){
-                    q.add(root.right);
+                else{
+                    temp =q.poll();
+                    if(temp.left!=null)q.add(temp.left);
+                    if(temp.right!=null)q.add(temp.right);
                 }
             }
         }
+
     }
-    
-    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.left.right = new Node(7);
+        lfview(root);
+    }
 }
