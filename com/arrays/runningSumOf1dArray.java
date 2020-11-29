0b8657c248b2f1e3e0b3de59b71a1f0e1f5ae872
Rabz2210 <topspin2210@gmail.com>
2020-11-29 23:49:47 +0530

    Initial Commit

---
+++ com/arrays/runningSumOf1dArray.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+//https://leetcode.com/problems/running-sum-of-1d-array/
+public class runningSumOf1dArray {
+    //public static int[] 
+
+    public static void main(String[] args) {
+        int[] test = new int[]{1,2,3,4};
+
+        for(int each:test){
+            System.out.print(each);
+        }
+    }
+}
