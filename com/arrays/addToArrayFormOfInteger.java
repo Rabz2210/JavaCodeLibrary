017389399dbfa9393ce1972410d3d02368dd4a8d
Rabz2210 <topspin2210@gmail.com>
2020-12-08 23:59:43 +0530

    Initial Commit

---
+++ com/arrays/addToArrayFormOfInteger.java
@@ -0,0 +1,28 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/add-to-array-form-of-integer/
+public class addToArrayFormOfInteger {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        int digit = 0;
+        int size = A.length;
+       for(int j = size-1;j>=0;j--){
+           if(A[j]==0){digit +=Math.pow(10, j);}
+           digit +=A[j]*Math.pow(10,j);
+       }
+       digit = digit +K;
+       
+       ArrayList<Integer>al = new ArrayList<Integer>();
+       while(digit!=0){
+           al.add(digit%10);
+           digit = digit/10;
+       }
+       return al;
+   }
+   public static void main(String[] args) {
+       int[] arr = new int[]{1,2,0,0};
+       int k = 34;
+       ArrayList
+}
