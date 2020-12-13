9719a714e8c0b568019fdfa2b24708a7b8fa5574
Rabz2210 <topspin2210@gmail.com>
2020-12-13 22:48:29 +0530

    Initial Commit

---
+++ com/hashing/sortingHashMap.java
@@ -0,0 +1,22 @@
+package com.hashing;
+
+import java.util.HashMap;
+
+public class sortingHashMap {
+
+
+    //sort hashmap based on keys
+    public static HashMap<Integer,Integer> sortBykeys(HashMap<Integer,Integer> hm){
+        return hm;
+    }
+
+    //sort hashmap based on values
+    public static HashMap<Integer,Integer> sortByValues(HashMap<Integer,Integer> hm){
+        return hm;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
