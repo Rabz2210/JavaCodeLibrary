71cb83b998ecb5cd412e25aee9259c68d5d2c7f7
Rabbani <topspin2210@gmail.com>
2022-01-17 19:30:43 +0530

    initial commit

---
+++ com/datastructures/sumDigitsToOne.java
@@ -0,0 +1,19 @@
+package com.datastructures;
+
+import java.util.Scanner;
+
+public class sumDigitsToOne {
+
+    public static int sumDigits(int input) {
+        if(input<10)return input;
+        int sum=0;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        int sum=0;
+        sumDigits(sum);
+    }
+}
