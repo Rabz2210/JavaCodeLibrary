df791525d8ca8e922d65f57c675936278a8efe16
Rabbani <topspin2210@gmail.com>
2022-01-21 23:25:43 +0530

    initial commit

---
+++ com/rough/DoubleNode.java
@@ -0,0 +1,13 @@
+package com.rough;
+
+public class DoubleNode {
+    public int data;
+    public DoubleNode left;
+    public DoubleNode right;
+    DoubleNode(){}
+    public DoubleNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
