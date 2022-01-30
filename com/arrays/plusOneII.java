df8608682eeeea90df7053ba759a8443209d0459
Rabbani <topspin2210@gmail.com>
2022-01-30 12:46:59 +0530

    Initial Commit

--- com/arrays/plusOneII.java
+++ com/arrays/plusOneII.java
@@ -1,12 +1,42 @@
 package com.arrays;
 
 public class plusOneII {
-    public int[] plusOne(int[] digits) {
+    public static int[] plusOne(int[] digits) {
+        boolean carryOn = false;
+        int length = digits.length-1;
+        int j = length+1;
         
-        return null;
+        do{
+            if((digits[length]+1)>9){
+                digits[length] = 0;
+                carryOn=true;
+            }else{
+                digits[length]+=1;
+                carryOn = false;
+            }
+            length--;
+        }while(carryOn && length>=0);
+        
+        if(carryOn){
+            int[] res = new int[j+1];
+            res[0] = 1;
+            int k=1;
+            for(int i=0;i<j;i++){
+                res[k] = digits[i];
+                k++;
+            }
+            return res;
+        }
+        return digits;
     }
 
+    
+
     public static void main(String[] args) {
-        
+        int[] arr= new int[]{9,9,9,9};
+        int[] res = plusOne(arr);
+        for(int each:res){
+            System.out.print(each+" ");
+        }
     }
 }
