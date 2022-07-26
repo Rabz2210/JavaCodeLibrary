b3b626db5bd0078bc1a08b09bba32eacbdfd0e28
Rabbani <topspin2210@gmail.com>
2022-07-26 08:46:52 +0530

    initial commit

---
+++ com/stack/stackPrac.java
@@ -0,0 +1,12 @@
+package com.stack;
+import java.util.*;
+
+public class stackPrac {
+    public static void main(String[] args) {
+        Integer temp = null;
+        temp = 0;
+        ++temp;
+        System.out.println(temp);
+        
+    }
+}
