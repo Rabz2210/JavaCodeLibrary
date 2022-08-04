85866853c93f1564c662880f1c83ae967deb5fa8
Rabbani <topspin2210@gmail.com>
2022-08-04 15:56:45 +0530

    initial commit

---
+++ com/rough/binarySearchTemplateII.java
@@ -0,0 +1,12 @@
+package com.rough;
+
+public class binarySearchTemplateII {
+    public static boolean binarySearch(){
+
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
